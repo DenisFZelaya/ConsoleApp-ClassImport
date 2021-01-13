947e64a5202af96fdb5f58e0e34583aa48a229d8
DenisFZelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-01-13 15:27:08 -0600

    Add project files.

---
+++ ConsoleApp-ClassImport/ClassPerson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+    public class ClassPerson
+    {
+		public int id;
+		public String nombre;
+		public String apellido;
+
+		// Constructor
+		public ClassPerson(int id, String nombre, String apellido)
+		{
+			this.id = id;
+			this.nombre = nombre;
+			this.apellido = apellido;
+		}
+
+
+	//public string getNombre() { return this.nombre}
+
+	public void mostrarInformacion()
+		{
+			Console.WriteLine("Datos Personales ");
+			Console.WriteLine("Id: " + this.id);
+			Console.WriteLine("Nombre: " + this.nombre);
+			Console.WriteLine("Apellido: " + this.apellido);
+		}
+	}
+
---
+++ ConsoleApp-ClassImport/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp_ClassImport
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ClassPerson p1 = new ClassPerson(5, "Denis", "Zelaya");
+            p1.mostrarInformacion();
+
+            //String nombrePersona = p1.getNombre();
+            //Console.WriteLine(nombrePersona);
+        }
+    }
+}
